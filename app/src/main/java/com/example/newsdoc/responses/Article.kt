7662dbdc32643d929139fcb53cd0fdd3cb845004
Jackline.ng'ang'a <ngendojackie2@gamil.com>
2023-03-28 17:51:50 -0700

    removed comments

--- app/src/main/java/com/example/newsdoc/responses/Article.kt
+++ app/src/main/java/com/example/newsdoc/responses/Article.kt
@@ -4,11 +4,9 @@ import com.google.gson.annotations.SerializedName
 
 data class Article(
     @SerializedName("author")
-    var author : String, // Marianne Dodson
+    var author : String,
     @SerializedName("content")
-    var content : String, // DC has a new clown prince of crime.Warner Bros. on Wednesday debuted the first trailer for Joker, its new film telling the iconic Batman villain's origin story. The trailer shows off Joaquin Phoenix's take on the character, including his suitably disturbing… [+1319 chars]
-
-
+    var content : String,
 ){
 
 }
